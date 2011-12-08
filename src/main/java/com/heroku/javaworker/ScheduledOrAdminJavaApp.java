12bb48e15bbe11911effc64611b001ecb8aa6a02
anandbn <anand@heroku.com>
2011-12-08 14:44:33 -0800

    Update src/main/java/com/heroku/javaworker/ScheduledOrAdminJavaApp.java

--- src/main/java/com/heroku/javaworker/ScheduledOrAdminJavaApp.java
+++ src/main/java/com/heroku/javaworker/ScheduledOrAdminJavaApp.java
@@ -8,24 +8,7 @@ public class ScheduledOrAdminJavaApp
 {
     public static void main( String[] args )
     {
-        try{
-
-            //initializeApplication
-
-            //performApplicationLogic
-
-        }catch(RuntimeException ex){
-
-            //tryToHandleTheError
-
-            //If error is not expected
-            //System.exit(APP_ERROR_CODE);
-
-        }finally{
-
-            //Do any aplication cleanup (closing db connections etc.)
-
-        }
+        System.out.println("Admin task run.");
     }    
 
 }
