2aa8a757fca210e624c604db1bb34ba1413b8084
Carl Hörberg <carl.hoerberg@gmail.com>
2012-03-28 18:57:53 +0200

    modified for CloudAMQP

--- src/main/java/OneOffProcess.java
+++ src/main/java/OneOffProcess.java
@@ -1,7 +1,25 @@
-public class OneOffProcess
-{
-    public static void main(String[] args)
-    {
-        System.out.println("OneOffProcess executed.");
-    }    
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.Channel;
+
+public class OneOffProcess {
+
+  private final static String QUEUE_NAME = "hello";
+
+  public static void main(String[] args) throws Exception {
+    String uri = System.getenv("CLOUDAMQP_URL");
+    if (uri == null) uri = "amqp://guest:guest@localhost";
+    ConnectionFactory factory = new ConnectionFactory();
+    factory.setUri(uri);
+    Connection connection = factory.newConnection();
+    Channel channel = connection.createChannel();
+
+    channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+    String message = "Hello CloudAMQP!";
+    channel.basicPublish("", QUEUE_NAME, null, message.getBytes());
+    System.out.println(" [x] Sent '" + message + "'");
+
+    channel.close();
+    connection.close();
+  }    
 }
--- src/main/java/WorkerProcess.java
+++ src/main/java/WorkerProcess.java
@@ -1,12 +1,30 @@
-public class WorkerProcess
-{
-    public static void main(String[] args)
-    {
-        while(true) {
-        	try { 
-        	    Thread.sleep(1000);
-            } catch(InterruptedException e) {}
-            System.out.println("Worker process woke up");
-        }
-    }    
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.QueueingConsumer;
+
+public class WorkerProcess {
+  private final static String QUEUE_NAME = "hello";
+
+  public static void main(String[] argv) throws Exception {
+    String uri = System.getenv("CLOUDAMQP_URL");
+    if (uri == null) uri = "amqp://guest:guest@localhost";
+
+    ConnectionFactory factory = new ConnectionFactory();
+    factory.setUri(uri);
+    Connection connection = factory.newConnection();
+    Channel channel = connection.createChannel();
+
+    channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+    System.out.println(" [*] Waiting for messages");
+
+    QueueingConsumer consumer = new QueueingConsumer(channel);
+    channel.basicConsume(QUEUE_NAME, true, consumer);
+
+    while (true) {
+      QueueingConsumer.Delivery delivery = consumer.nextDelivery();
+      String message = new String(delivery.getBody());
+      System.out.println(" [x] Received '" + message + "'");
+    }
+  }
 }
